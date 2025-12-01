d93ef05a40ab6589e9248af44ddb99f4f0752369
TheRABU <mdfazlerabbi114@gmail.com>
2025-12-01 18:13:28 +0600

    configured server and package.json, tsconfig now the server is running

---
+++ src/server.ts
@@ -0,0 +1,67 @@
+import type { Server } from "http";
+import app from "./app";
+
+let server: Server | undefined;
+const PORT = process.env.PORT || 5000;
+
+async function startServer() {
+  try {
+    server = app.listen(PORT, () => {
+      console.log("Server started and running OK!");
+    });
+  } catch (error) {
+    console.log("could not start the server", error);
+  }
+}
+
+(async () => {
+  startServer();
+})();
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal recieved... Server shutting down..");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT signal recieved... Server shutting down..");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejecttion detected... Server shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
